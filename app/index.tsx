16df6a0dce136c6c08abca7f84bfc9b889502730
Mariana Lino <marianalinosdev@gmail.com>
2025-03-17 17:42:22 -0300

    finalizando a configuração do nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl text-blue-500 font-bold">Filmes são demais!</Text>
+      <Text className="text-4xl text-accent font-bold">Filmes são demais!</Text>
     </View>
   );
 }
