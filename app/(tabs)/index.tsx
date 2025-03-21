6208b32f3e71b41c38ac4e6ab633accb753467d2
Mariana Lino <marianalinosdev@gmail.com>
2025-03-21 10:40:43 -0300

    feat: estilizando e organizando a barra de icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-4xl text-accent font-bold">Filmes são demais!</Text>
-      <Link href="/onboarding">Onboarding</Link>
     </View>
   );
 }
